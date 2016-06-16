ca0f5cf6e63003aecf46ce3892157b9038ca72b9
fi1446 <kamedashigeru@gmail.com>
2016-06-16 11:46:16 +0900

    ページ226まで ページネーションをつけました。
    問題点：
    ・参加ボタンを押してもポップアップが出てこない。機能自体は死んでいないので、確認ポップアップが出ないのはjavascriptの問題だと思う。
    ・エラー時に日本語が出ない。jaが機能していないと思う。
    ・ページの構成がおかしいのはCSSが変なだけだから優先度は低い。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,9 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_user, :logged_in?
 
+  rescue_from Exception, with: :error500
+  rescue_from ActiveRecord::RecordNotFound, ActionController::RoutingError, with: :error404
+
   private
 
   def current_user
