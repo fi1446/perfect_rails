e1c0be1f52f326a6bb86e4ecc80885eecd7e03ca
fi1446 <kamedashigeru@gmail.com>
2016-07-03 14:44:56 +0900

    Fix welcome_controller, fix welcome_index
    ransackを使わないため、関係記述を排除
    現時点でバグがkaminariの導入によるものだと判明、ここから調査する

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,16 +1,5 @@
 class WelcomeController < ApplicationController
-    PER = 10
-
     def index
-        @q = Event.page(params[:page]).per(PER).order(:start_time).ransack(search_params)
-        @events = @q.result
-    end
-
-    private
-
-    def search_params
-        params.require(:q).permit(:name_cont, :start_time_gteq)
-    rescue
-        { start_time_gteq: Time.zone.now }
+      @events = Event.where('start_time > ?', Time.zone.now).order(:start_time)
     end
 end
